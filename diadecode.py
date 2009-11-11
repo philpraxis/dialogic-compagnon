92f7f13c0be3f1f55ddc258e61508de62fa51de7
Philippe Langlois <Philippe.Langlois@gmail.com>
2009-11-11 23:40:04 +0100

    Refactored DB of protocol definition in its own class

--- diadecode.py
+++ diadecode.py
@@ -21,35 +21,86 @@ class Usage(Exception):
 	def __init__(self, msg):
 		self.msg = msg
 
+class db(object):
+   """Class that stores the Definition DB of Dialogic documents.
+   The Definition DB are cut and paste directly from the PDF into XXX.db file.
+   XXX is the name of the protocol being described, and the title of the Dialogic documentself."""
+   def __init__(self):
+      self.debug = True
+      super(db, self).__init__()
+      self.list = glob.glob("*.db")
+      self.contents = {}
+      
+   def find_msg(self, msg_type):
+      """docstring for db_find_msg"""
+      for db in self.list:
+         if self.debug: print db
+         content = open(db).readlines()
+
+         self.contents[db] = content            # Cache the read content so that we can accelerate access later
+
+         for index,line in enumerate(content):
+            # print line
+            if "(0x%s)"%msg_type in line:
+               print line
+               if line.split(' ')[0] == "type":
+                  print "Found definition: %s in %s" % (line, db)
+                  if  "FIELD NAME MEANING" in content[index-1]:
+                     print "This is a name-list definition"
+                  else:
+                     print "This is possible a position definition: %s" % content[index-1]
+                  return
+
+
 class Message(object):
    """Process Messages from Dialogic stack"""
-   def __init__(self, message, arg_debug=True):
+   def __init__(self, message, arg_debug=False):
       super(Message, self).__init__()
       if arg_debug == True:
          self.debug = True
       else:
          self.debug = False
       if self.debug: print "class Message(object): %s" % message
+
+      # self.db_list()
+      self.db = db()
+
       self.message = message
+
       parts = self.message.split('-')
       self.msg_type = parts.pop(0)
       if self.debug: print "self.msg_type=%s" % self.msg_type
       if self.debug: print "parts=%s" % parts
+
       self.elements = {}
       for part in parts:
          element_key = part[0:1]
          self.elements[element_key] = part[1:]
          if self.debug: print "%s:%s" % (element_key, part[1:])
          
-   def db_list(self):
-      """docstring for db_list"""
-      self.db = glob.glob(’*.db’)
+   # def db_list(self):
+   #    """docstring for db_list"""
+   #    self.db = glob.glob("*.db")
    
-   def db_find_msg(self, message):
+   def db_find_msg(self, msg_type = False):
       """docstring for db_find_msg"""
-      pass
-   
-   
+      if msg_type == False:
+         msg_type = self.elements['t']
+
+      for db in self.db.list:
+         if self.debug: print db
+         content = open(db).readlines()
+         for index,line in enumerate(content):
+            # print line
+            if "(0x%s)"%msg_type in line:
+               print line
+               if line.split(' ')[0] == "type":
+                  print "Found definition: %s in %s" % (line, db)
+                  if  "FIELD NAME MEANING" in content[index-1]:
+                     print "This is a name-list definition"
+                  else:
+                     print "This is possible a position definition: %s" % content[index-1]
+                  return
 
 def main(argv=None):
 	if argv is None:
@@ -79,6 +130,9 @@ def main(argv=None):
 
 	# Do stuff here?
 	print "Payload:" + message
+	msg.db_find_msg()
+	print "NEW>>>>>"
+	msg.db.find_msg(msg.elements['t'])
 
 if __name__ == "__main__":
 	sys.exit(main())
